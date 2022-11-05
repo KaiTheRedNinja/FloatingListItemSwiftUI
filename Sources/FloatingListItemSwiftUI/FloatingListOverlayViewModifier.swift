ec770aa0d1612992c40791beadd037b4441e9618
Tay Kai Quan <kaitay303@gmail.com>
2022-11-05 16:54:06 +0800

    Allow pinning to the top

--- Sources/FloatingListItemSwiftUI/FloatingListOverlayViewModifier.swift
+++ Sources/FloatingListItemSwiftUI/FloatingListOverlayViewModifier.swift
@@ -14,9 +14,12 @@ struct FloatingListOverlayViewModifier<Body: View>: ViewModifier {
     @ViewBuilder
     var hoverContent: () -> Body
 
-    init(floaterID: String, @ViewBuilder content: @escaping () -> Body) {
+    @State var pinLocations: PinLocations
+
+    init(floaterID: String, pinLocations: PinLocations = .bottom, @ViewBuilder content: @escaping () -> Body) {
         self.itemManager = .manager(string: floaterID)
         self.hoverContent = content
+        self._pinLocations = State(initialValue: pinLocations)
     }
 
     func body(content: Content) -> some View {
@@ -26,27 +29,31 @@ struct FloatingListOverlayViewModifier<Body: View>: ViewModifier {
                     GeometryReader { geometry in
                         VStack {
                             Spacer().frame(height: 20)
-                            hoverContent()
-                                .onChange(of: geometry.frame(in: .global)) { newValue in
-                                    itemManager.pos = newValue
-                                }
-                                .onAppear {
-                                    itemManager.pos = geometry.frame(in: .local)
-                                }
-                                .background(tableColor)
-                                .cornerRadius(10)
-                                .padding(.horizontal, 20)
-                                .shadow(color: Color.gray, radius: itemManager.topShadow * 10)
-                                .opacity(itemManager.floatingTop ? 1 : 0)
+                            if pinLocations == .top || pinLocations == .all {
+                                hoverContent()
+                                    .onChange(of: geometry.frame(in: .global)) { newValue in
+                                        itemManager.pos = newValue
+                                    }
+                                    .onAppear {
+                                        itemManager.pos = geometry.frame(in: .local)
+                                    }
+                                    .background(tableColor)
+                                    .cornerRadius(10)
+                                    .padding(.horizontal, 20)
+                                    .shadow(color: Color.gray, radius: itemManager.topShadow * 10)
+                                    .opacity(itemManager.floatingTop ? 1 : 0)
+                            }
 
                             Spacer()
 
-                            hoverContent()
-                                .background(tableColor)
-                                .cornerRadius(10)
-                                .padding(.horizontal, 20)
-                                .shadow(color: Color.gray, radius: itemManager.bottomShadow * 10)
-                                .opacity(itemManager.floatingBottom ? 1 : 0)
+                            if pinLocations == .bottom || pinLocations == .all {
+                                hoverContent()
+                                    .background(tableColor)
+                                    .cornerRadius(10)
+                                    .padding(.horizontal, 20)
+                                    .shadow(color: Color.gray, radius: itemManager.bottomShadow * 10)
+                                    .opacity(itemManager.floatingBottom ? 1 : 0)
+                            }
                             Spacer().frame(height: 20)
                         }
                     }
@@ -60,8 +67,17 @@ struct FloatingListOverlayViewModifier<Body: View>: ViewModifier {
     }
 }
 
+public enum PinLocations {
+    case top
+    case bottom
+    case all
+    case none
+}
+
 public extension List {
-    func floatingList(floaterID: String, @ViewBuilder body: @escaping () -> some View) -> some View {
-        self.modifier(FloatingListOverlayViewModifier(floaterID: floaterID, content: body))
+    func floatingList(floaterID: String,
+                      pinLocations: PinLocations = .bottom,
+                      @ViewBuilder body: @escaping () -> some View) -> some View {
+        self.modifier(FloatingListOverlayViewModifier(floaterID: floaterID, pinLocations: pinLocations, content: body))
     }
 }
